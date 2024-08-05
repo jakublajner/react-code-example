9a478ed3fc4d2b8357957c7603bc636b05af673f
Jakub Lajner <35150027+jakublajner@users.noreply.github.com>
2024-08-05 12:58:56 +0200

    Add material ui (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Container, CssBaseline, ThemeProvider, Typography } from "@mui/material";
+
+import theme from "./theme";
+
 function App() {
-  return <div>Hi there!</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <Container>
+        <Typography variant="h1" color="primary">
+          Hi there!
+        </Typography>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
