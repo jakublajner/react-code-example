eb74894fae951863b99b5c5d3efa2df14e2cd487
Jakub Lajner <35150027+jakublajner@users.noreply.github.com>
2024-08-05 09:24:55 +0200

    Add webpack config with ts and react (#1)

---
+++ webpack.config.cjs
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: false,
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+};
