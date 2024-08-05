d6b370d59a80f91ef3da087096102671c971c21e
Jakub Lajner <jakub.lajner@gmail.com>
2024-08-05 16:03:25 +0200

    Add header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,106 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import Link from "@mui/material/Link";
+import { useState } from "react";
+
+import { paths } from "../../routes/paths";
+import { RouterLink } from "../router-link";
+
+export default function Header() {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const container = window !== undefined ? () => window.document.body : undefined;
+  const navItems = [
+    { key: "posts", label: "Posts", path: paths.posts },
+    { key: "statisctis", label: "Statistics", path: "#" },
+  ];
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Posts App
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.key} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }} component={RouterLink} href={item.path}>
+              <ListItemText primary={item.label} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <>
+      <AppBar component="nav" position="relative">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Posts App
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "flex", gap: 6 } }}>
+            {navItems.map((item) => (
+              <Link
+                key={item.key}
+                component={RouterLink}
+                href={item.path}
+                color="#fff"
+                underline="none"
+              >
+                {item.label}
+              </Link>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": { boxSizing: "border-box", width: 240 },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </>
+  );
+}
